7b4aa36b2d83d36b5891e40c7c7f63a9f8f61a62
Angelamelissamapuranga <angelamapuranga68@gmail.com>
2026-01-18 05:40:46 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by Angela Mapuranga and it is open sourced
+      </footer>
+    </div>
+  );
+}
